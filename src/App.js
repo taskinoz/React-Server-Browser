cb30b3924d8ea91ce50418c3011250815d96b83a
Tristan Kennedy <tristan@taskinoz.com>
2022-07-04 19:48:32 +1000

    Set up filter system

--- src/App.js
+++ src/App.js
@@ -44,26 +44,54 @@ const Row = styled.tr`
   ${props => props.active?"height: 100px":""}
 `;
 
+//Comparer Function    
+const GetSortOrder = (prop) => {    
+  return (a, b) => {    
+    if (a[prop] > b[prop]) {    
+        return 1;    
+    } else if (a[prop] < b[prop]) {    
+        return -1;    
+    }    
+    return 0;  
+  }    
+}
+
 const checkPassword = (e, password) => {
-    if (password) {
-      e.preventDefault()
-      let serverPassword = prompt("Enter Password:");
-      window.location.href = `${e.target.href}:${btoa(serverPassword)}`;
-    }
-    window.location.href = e.target.href;
+  if (password) {
+    e.preventDefault()
+    let serverPassword = prompt("Enter Password:");
+    window.location.href = `${e.target.href}:${btoa(serverPassword)}`;
   }
+  window.location.href = e.target.href;
+}
 
-  const description = (desc, active) => {
-    if (desc.length > 100 && !active) {
-      return desc.slice(0,100)+"..."
-    }
-    return desc
+const description = (desc, active) => {
+  if (desc.length > 100 && !active) {
+    return desc.slice(0,100)+"..."
   }
+  return desc
+}
 
+
+const filterServers = (filter, direction, array) => {
+  if (filter) {
+    if (direction === "asc") {
+      return array.sort(GetSortOrder(filter))
+    }
+    else if (direction === 'desc') {
+      array.sort(GetSortOrder(filter))
+      return array.reverse();
+    }
+  }
+  return array;
+}
 
 function App() {
   const [servers, setServers] = useState([]);
   const [active, setActive] = useState("");
+  const [filter, setFilter] = useState("");
+  const [filterDirection, setFilterDirection] = useState("asc");
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await fetch('https://northstar.tf/client/servers')
@@ -75,6 +103,12 @@ function App() {
     }, 1000*30)
     fetchData()
   },[])
+
+  const updateFilters = (filterName) => {
+    if (filter === filterName ) {
+      
+    }
+  }
 
   const makeActive = (index) => {
     if (active === index) {
@@ -109,23 +143,27 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {servers.map((server,index)=>
-          <Row active={active == index} key={index} onClick={() => makeActive(index)}>
-            <td className='password'>{server.hasPassword && <FontAwesomeIcon icon={faLock} />}</td>
-            <td className='server'>{server.name}</td>
-            <td className='description'>{description(server.description, active == index)}</td> 
-            <td className='players'>{server.playerCount}/{server.maxPlayers}</td> 
-            <td className='map'>{maps[server.map] ?? server.map}</td> 
-            <td className='mode'>{gamemodes[server.playlist]?? server.playlist}</td>
-            <td className='communities'><Communities desc={server.description} /></td>
-            <td className='join'>
-              <a 
-                href={`northstar://server@${server.id}`}
-                onClick={(e) => checkPassword(e,server.hasPassword)}
-              ><FontAwesomeIcon icon={faPlay} /></a>
-            </td>
+          {(filterServers(filter, filterDirection, servers)).map((server,index)=> {
+            let isActive = (active === index);
+            return (
+              <Row active={isActive} key={index} onClick={() => makeActive(index)}>
+                <td className='password'>{server.hasPassword && <FontAwesomeIcon icon={faLock} />}</td>
+                <td className='server'>{server.name}</td>
+                <td className='description'>{description(server.description, isActive)}</td> 
+                <td className='players'>{server.playerCount}/{server.maxPlayers}</td> 
+                <td className='map'>{maps[server.map] ?? server.map}</td> 
+                <td className='mode'>{gamemodes[server.playlist]?? server.playlist}</td>
+                <td className='communities'><Communities desc={server.description} /></td>
+                <td className='join'>
+                  <a 
+                    href={`northstar://server@${server.id}`}
+                    onClick={(e) => checkPassword(e,server.hasPassword)}
+                  ><FontAwesomeIcon icon={faPlay} /></a>
+                </td>
 
-          </Row>
+              </Row>
+            );
+          }
           )}
         </tbody>
       </StyledTable>}
